5211d2e0fd71540644191c0c4f4754810302c79f
KingVik-Planet <chukwukingsley56@gmail.com>
2023-03-17 07:37:53 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Numberz
+ * Date: 17032023
+ * author: KingVik-Planet
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int kingsley;
+
+	for (kingsley  = 48; kingsley < 58; kingsley++)
+		putchar(kingsley);
+	putchar('\n');
+	return (0);
+}
