2143e67fb9ae1b8a541b53faf9e334edab7cff92
KingVik-Planet <chukwukingsley56@gmail.com>
2023-03-17 06:54:40 -0700

    print both uppercase and lowercase aplhabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print both uppercase and lowercase aplhabets
+ * Author: KingVik-Planet
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	char kp;
+	char kgp;
+
+	for (kp = 'a'; kp <= 'z'; kp++)
+		putchar(c);
+	for (kgp = 'A'; kgp <= 'Z'; kgp++)
+		putchar(kgp);
+	putchar('\n');
+	return (0);
+}
