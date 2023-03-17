ad44fdd960c9a3e99aaa77ba880279e3eecd82b6
KingVik-Planet <chukwukingsley56@gmail.com>
2023-03-17 06:39:12 -0700

    Printing alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -Program that prints the alphabet in lowercase
+ * Description: I will use my full name
+ * Author: KingVik-Planet
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	char kingsley;
+
+	for (kingsley = 'a'; kingsley <= 'z'; kingsley++)
+	{
+		putchar(kingsleychikachukwu);
+	}
+	putchar('\n');
+	return (0);
+}
