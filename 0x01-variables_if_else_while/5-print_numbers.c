c13df35dba197f15932ca6de2a3340bfc5b165bb
KingVik-Planet <chukwukingsley56@gmail.com>
2023-03-17 07:29:00 -0700

    prints all single digit numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author: sammy
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	int kingsley;
+
+	for (kingsley = 0; kingsley < 10; kingsley++)
+	{
+		printf("%d", kingsley);
+	}
+	printf("\n");
+	return (0);
+}
