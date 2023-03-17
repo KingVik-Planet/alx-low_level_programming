3a97c8ff8cad9b1f134ab2cf8f0f16c71f630e10
KingVik-Planet <chukwukingsley56@gmail.com>
2023-03-17 07:16:10 -0700

    program that prints the alphabet in lowercase

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,19 +2,19 @@
 
 /**
  * main - Entry point
- * Author: KingVik-Planet
- * Date: 17-03-2023
+ * author: KingVik-Planet
  * Description: program that prints the alphabet in lowercase
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        char kingsley;
+	char kingsley;
 
-        for (kingsley = 'a'; kingsley <= 'z'; kingsley++)
-        {
-                if (kingsley != 'q' && kingsley != 'e')
-                        putchar(kingsley);
-        }
-        putchar('\n');
-        return (0);
+	for (kingsley = 'a'; kingsley <= 'z'; kingsley++)
+	{
+		if (kingsley != 'q' && kingsley != 'e')
+			putchar(kingsley);
+	}
+	putchar('\n');
+	return (0);
+}
