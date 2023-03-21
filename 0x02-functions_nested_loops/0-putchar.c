4879517bf74ba7c1dfcf087f3ec5f69bdf80b1d0
KingVik-Planet <chukwukingsley56@gmail.com>
2023-03-21 06:30:21 -0700

    program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,9 +5,9 @@
  * _putchar - Entry point
  * @c: The character to print
  * Description: Progrm
- * Return: nothing, return void
+ * Return: On success 1
+ * O error, 1 is returned, and errno is set appropriately.
  */
-
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
