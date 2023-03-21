071c7c9301f224a9611ad6ce6d5a8b3ec0c59e96
KingVik-Planet <chukwukingsley56@gmail.com>
2023-03-21 05:13:31 -0700

    program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,22 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
- * main - prints _putchar, followed by a new linees
- * Return: Alwys 0 (Success)
+ * main - Entry point of the program.
+ * Description: write a program that prints "_putchar" followed by a new line.
+ * Return: Always 0 (Success.)
  */
 int main(void)
 {
-	char abe[] = "_putchar";
-
-	intc;
-
-	for (c = 0; c < 8; c++)
-	{
-		_putchar(abe[c]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
