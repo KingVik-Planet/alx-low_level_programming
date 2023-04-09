83f47543bc8b327dba6bcf765c33e8afae58f9c0
KingVik-Planet <chukwukingsley56@gmail.com>
2023-04-09 04:17:32 -0700

    Create the static library libmy.a containing all the functions

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -13,22 +13,4 @@ int _isupper(int c)
 	else
 		return (0);
 
-#include "main.h"
-
-/**
- * _isupper - checks uppercase
- * @c: parameter
- * Return: 0 if sucessful
- */
-
-int _isupper(int c)
-{
-	if (c >= 65 && c < 91)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-}}
+}
